69ca05ac619d585d89763b28bf79f6d03a9179ab
mavibo <96347083+mavibo@users.noreply.github.com>
2023-05-19 10:35:58 -0500

    creacion del microservicio 1

---
+++ Backend/microservico1/spring-boot-microservicio-1-hoja-de-servicio/src/main/java/com/bonier/springbootmicroservicio1hoja/de/servicio/SpringBootMicroservicio1HojaDeServicioApplication.java
@@ -0,0 +1,13 @@
+package com.bonier.springbootmicroservicio1hoja.de.servicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicio1HojaDeServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservicio1HojaDeServicioApplication.class, args);
+	}
+
+}
---
+++ Backend/microservico1/spring-boot-microservicio-1-hoja-de-servicio/src/test/java/com/bonier/springbootmicroservicio1hoja/de/servicio/SpringBootMicroservicio1HojaDeServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonier.springbootmicroservicio1hoja.de.servicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicio1HojaDeServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
