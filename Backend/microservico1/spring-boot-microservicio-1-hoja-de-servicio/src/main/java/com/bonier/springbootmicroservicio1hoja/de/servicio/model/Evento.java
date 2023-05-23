9693232bdf1c5fe5530ad8ed340c5eb7ef50d154
mavibo <96347083+mavibo@users.noreply.github.com>
2023-05-23 10:25:22 -0500

    agregando itens a la entidad evento

--- Backend/microservico1/spring-boot-microservicio-1-hoja-de-servicio/src/main/java/com/bonier/springbootmicroservicio1hoja/de/servicio/model/Evento.java
+++ Backend/microservico1/spring-boot-microservicio-1-hoja-de-servicio/src/main/java/com/bonier/springbootmicroservicio1hoja/de/servicio/model/Evento.java
@@ -25,37 +25,60 @@ public class Evento {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    //fecha creacion del evento
     @Column(name = "creation_date")
     private LocalDateTime creationDate;
-
+    
+    //fecha inicio de evento por el colaborador
     @Column(name = "start_date")
     private LocalDateTime startDate;
 
+    //fecha finalizacion del evento
     @Column(name = "end_date")
     private LocalDateTime endDate;
 
+    //estado del evento pendiente, encurso, finalizado
     @Column(name = "status")
     private String status;
 
+    //nombre del evento
     @Column(name = "name")
     private String name;
 
+    //descripcion del evento por el cliente
     @Column(name = "description")
     private String description;
 
+    //fotografia del problema del cliente si es necesario
     @Column(name = "picture")
     private String picture;
 
+    //forma de realizacion de online u offline
     @Column(name = "type")
     private String offon;
 
+    //responsable de darle solucion
     @Column(name = "collaborator")
     private String collaborator;
 
+    //quine realiza asigan el evento
     @Column(name = "leader")
     private String leader;
 
+    //prioridad
     @Column(name = "level")
     private String level;
+
+    //nota del colaborador
+    @Column(name = "note")
+    private String note;
+
+    //solaborador de apoyo
+    @Column(name = "support")
+    private String support;
+
+    //finalizacion del evento finalizacion bien, incompleto, mal
+    @Column(name = "completion_status")
+    private String completion_status;
 
 }
